59dd424d296fd75811dc474c25e48f81428c230d
Santiago Gaviria <santiago@junior-2.local>
2009-11-09 18:51:00 -0500

    code depuration III

--- sinatra/app/public/javascripts/mi_tienda.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  $mi_tienda = $.mi_tienda = function(settings) {
-  };
-
-  $.fn.mi_tienda = function(settings) {
-    settings = $.extend( {}, $mi_tienda.defaults, settings );
-    //    $(this).find(".product_form").product_form();
-  };
- })(jQuery);
